ab40da5ab990aae580c739c85364e4fc12204db1
Deepak-Aggarwal-Chicmic <deepak.aggarwal@chicmicstudios.in>
2024-11-12 14:03:02 +0530

    SAve

--- app/routes/widgets/route.jsx
+++ app/routes/widgets/route.jsx
@@ -35,7 +35,8 @@ export const loader = async ({ request }) => {
     return {
       id: widget.id,
       name: widget.name,
-      description: widget.description,
+      description_title: widget.description_title,
+      description_content: widget.description_content,
       image: widget.image,
       categoryId: widget.categories.map(item => item.id),
       isFavorite,
