091e379a137a2b6a308529453dd234eb0a76c98e
Deepak-Aggarwal-Chicmic <deepak.aggarwal@chicmicstudios.in>
2024-11-14 09:55:18 +0530

    Save

--- app/components/atoms/AppsRenderList/index.jsx
+++ app/components/atoms/AppsRenderList/index.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Badge, Link, ResourceItem, ResourceList } from '@shopify/polaris';
+import {  Badge, Link, ResourceItem, ResourceList } from '@shopify/polaris';
 import React from 'react';
 import './style.css';
 
--- app/routes/app.upgrade.jsx
+++ app/routes/app.upgrade.jsx
@@ -5,7 +5,7 @@ export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const queryParams = new URLSearchParams(url.search);
   const plan = queryParams.get('plan');
-  const { billing, session } = await authenticate.admin(request);
+  const { billing } = await authenticate.admin(request);
  
   await billing.require({
     plans: [plan],
