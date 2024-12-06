50828b5503eca70eb7252d11dcbd65081483925f
lakhvir2024 <lakhvir.singh@chicmic.co.in>
2024-12-06 16:15:58 +0530

    style: popup ui fixes

--- app/components/atoms/sliderModal/index.jsx
+++ app/components/atoms/sliderModal/index.jsx
@@ -1,18 +1,18 @@
 import { Button, Frame, Modal, TextContainer } from "@shopify/polaris";
 import { useState, useCallback } from "react";
-
+import "./style.css"
 export function SliderModal({
   active,
   toggleModal = () => {},
   toRender = <></>,
 }) {
   return (
-    <div style={{ height: "500px" }}>
-      <Frame>
-        <Modal open={active} onClose={toggleModal}>
-          <Modal.Section>{toRender}</Modal.Section>
+    <div >
+      {/* <Frame> */}
+        <Modal open={active} onClose={toggleModal} className="ftgvghvjhvj">
+          <Modal.Section><div className="slider-modal">{toRender}</div></Modal.Section>
         </Modal>
-      </Frame>
+      {/* </Frame> */}
     </div>
   );
 }
