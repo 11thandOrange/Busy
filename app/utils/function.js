394e086d18aca9c53d718082fc25d9c1c8c7c10c
Deepak-Aggarwal-Chicmic <deepak.aggarwal@chicmicstudios.in>
2024-12-10 11:38:48 +0530

    Cart Notice

--- app/utils/function.js
+++ app/utils/function.js
@@ -373,7 +373,10 @@ export const getCartNotice = async (shop) => {
   {
     cartNotice.general_setting = cartNotice.general_setting?JSON.parse(cartNotice.general_setting):''
     htmlToInsert = `<div id="busyBuddyCartNotice" class="di-flex busyBuddyCartNotice" style="background-color:${cartNotice.backgroundColor};color:${cartNotice.textColor};margin-top:${cartNotice.general_setting.marginTop}${cartNotice.general_setting.marginTopUnit};margin-bottom:${cartNotice.general_setting.marginBottom}${cartNotice.general_setting.marginBottomUnit};">`;
-    htmlToInsert += `<div class="fireEmoji">${cartNotice.emojiToAdd}</div>`
+    if(cartNotice.emojiToAdd!=null)
+    {
+      htmlToInsert += `<div class="fireEmoji">${cartNotice.emojiToAdd}</div>`
+    }
     if (cartNotice.showCountdown) {
       const countdownTimer = parseInt(cartNotice.countdown_timer)*60;
       let minutes = Math.floor(countdownTimer / 60);
