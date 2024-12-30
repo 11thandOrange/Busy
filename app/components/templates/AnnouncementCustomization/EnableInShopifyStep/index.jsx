399996e388adb24f586afddd9b2f1eb6a18d428d
Jasmeet-Chicmic <jasmeet.sidhu@chicmic.co.in>
2024-12-30 12:06:05 +0530

    Added : App enable link

--- app/components/templates/AnnouncementCustomization/EnableInShopifyStep/index.jsx
+++ app/components/templates/AnnouncementCustomization/EnableInShopifyStep/index.jsx
@@ -4,7 +4,8 @@ import ToggleButtons from "../../../atoms/ToggleButtons";
 import { updateState } from "../../../../utils/clientFunctions";
 import ImageRenderer from "../../../atoms/ImageRenderer";
 import IMAGES from "../../../../utils/Images";
-import "./style.css"
+import "./style.css";
+import { APP_LINKS } from "../../../../utils/constants";
 const EnableAppToggles = {
   Enable_Later: { name: "Enable Later", id: 0 },
   Enable_Now: { name: "Enable Now", id: 1 },
@@ -20,28 +21,28 @@ const EnableInShopifyStep = ({
   return (
     <div className="Announcement-box">
       <Card>
-        <Text>
-          Your Announcement Bar Won’t Show Up In Your Store Yet!
-        </Text>
-        <p className="desc">Complete Install & Your BusyBuddy Apps Will Show On Your Store. You will only need to complete this step once</p>
+        <Text>Your Announcement Bar Won’t Show Up In Your Store Yet!</Text>
+        <p className="desc">
+          Complete Install & Your BusyBuddy Apps Will Show On Your Store. You
+          will only need to complete this step once
+        </p>
         <div className="button-container">
-        <Button
-          variant="primary"
-          onClick={() => {
-            console.log("appActivationInStoreLink", enableAppInStoreURL);
+          <Button
+            variant="primary"
+            onClick={() => {
+              console.log("appActivationInStoreLink", enableAppInStoreURL);
 
-            window.open(enableAppInStoreURL, "_blank");
-          }}
-        >
-          Enable In Your Theme Editor
-        </Button>
-        
+              window.open(enableAppInStoreURL, "_blank");
+            }}
+          >
+            Enable In Your Theme Editor
+          </Button>
         </div>
-        
+
         <div className="image-container">
           <ImageRenderer src={IMAGES.AppInstall}></ImageRenderer>
         </div>
-        <div className='link-foot'>
+        <div className="link-foot">
           <Checkbox
             label="Enable later"
             onChange={(value) => {
@@ -52,7 +53,9 @@ const EnableInShopifyStep = ({
             checked={settingsState.enableAppInShopifyLater || false}
           />
           <div className="link-container">
-            <a href="#">How To Enable BusyBuddy In Your Shopify Store</a>
+            <a href={APP_LINKS.HOW_TO_ENABLE_LINK} target="_blank">
+              How To Enable BusyBuddy In Your Shopify Store
+            </a>
           </div>
         </div>
       </Card>
